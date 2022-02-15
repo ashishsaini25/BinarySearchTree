eee2b02565984b8e212597f37b71b57bcc086806
ghost331 <67087070+ghost331@users.noreply.github.com>
2022-02-15 23:28:14 +0530

    [Ashish] Add:Binary search Tree Created

---
+++ BinaryTree/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTree
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            BinarySearchTree searchTree = new BinarySearchTree();
+            searchTree.Add(56);
+            searchTree.Add(30);
+            searchTree.Add(70);
+            searchTree.Display();
+
+
+        }
+    }
+}
